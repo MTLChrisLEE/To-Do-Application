e7b9eff9d7e9abf44490570dfbcd3e7e9edcd6a1
MTLChrisLEE <sanghyukchrislee@gmail.com>
2017-08-04 16:11:47 -0400

    First commit
    Renames the mail FXML file
    Creates a new package
    Creates a list of TodoItem

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import sample.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    private List<TodoItem> todoItems;
+
+    public void initialize(){
+        TodoItem item1=new TodoItem("This is ToDoApplication","Please enjoy", LocalDate.of(2017, Month.AUGUST,04));
+        TodoItem item2=new TodoItem("First JavaFX","----", LocalDate.of(2017,Month.AUGUST,04));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+    }
+
+
+
+
+
 }
